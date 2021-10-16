67e2172961bd924ec365a9777185bdde390ac686
Prajwal <prajwal.zurunge12@gmail.com>
2021-10-16 23:08:51 +0530

    Welcome to Logical Problems

---
+++ LogicalProblems-Day6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalProblems_Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Logical Problems ");
+        }
+    }
+}
